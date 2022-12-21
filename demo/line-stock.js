b81f1eb59b281515f232433d411ad45bb8729c0a
Guy Baudouin <guythoun@gmail.com>
2022-12-21 12:05:58 +0100

    add overlap demos

--- demo/line-stock.js
+++ demo/line-stock.js
@@ -1,4 +1,6 @@
 let lineStock;
+let lineStockOverlap;
+let lineStockVerticalOverlap;
 
 function initLineStock() {
     lineStock = new LineStock(cardsManager, document.getElementById('line-stock'), {
@@ -28,4 +30,28 @@ function addCardToLineStockWithAnimation(fromElement, customAnimation) {
         { id: getCardId(), type: 1 + Math.floor(Math.random() * 4), type_arg: 1 + Math.floor(Math.random() * 10), location: 'table', location_arg: 0 },
         animationSettings
     );
+}
+
+function initLineStockOverlap() {
+    lineStockOverlap = new LineStock(cardsManager, document.getElementById('line-stock-overlap'));
+
+    // add cards
+    lineStockOverlap.addCards([
+        { id: getCardId(), type: 3, type_arg: 2, location: 'table', location_arg: 0 },
+        { id: getCardId(), type: 1, type_arg: 5, location: 'table', location_arg: 0 },
+        { id: getCardId(), type: 1, type_arg: 6, location: 'table', location_arg: 0 },
+    ]);
+}
+
+function initLineStockVerticalOverlap() {
+    lineStockVerticalOverlap = new LineStock(cardsManager, document.getElementById('line-stock-vertical-overlap'), {
+        direction: 'column',
+    });
+
+    // add cards
+    lineStockVerticalOverlap.addCards([
+        { id: getCardId(), type: 3, type_arg: 2, location: 'table', location_arg: 0 },
+        { id: getCardId(), type: 1, type_arg: 5, location: 'table', location_arg: 0 },
+        { id: getCardId(), type: 1, type_arg: 6, location: 'table', location_arg: 0 },
+    ]);
 }
